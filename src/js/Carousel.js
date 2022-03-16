4031d1d87ebfee16bbbee45fadb7f655a7d721c2
emmanuel cuevas <emmanuelcuevasm1@gmail.com>
2022-03-16 19:00:49 -0400

    minor changes for slick js

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -1,12 +1,4 @@
 $(document).on('ready',function(){
-    $(".myCarousel").slick({
-
-          dots: true,
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 3,
-          speed: 200,
-          
-      });
+    
 
    });
