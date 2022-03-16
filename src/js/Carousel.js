1a46c0331c22d9e2c8e2d8a622643ed860471c0c
emmanuel cuevas <emmanuelcuevasm1@gmail.com>
2022-03-16 12:12:58 -0400

    slick js installedd

---
+++ src/js/Carousel.js
@@ -0,0 +1,12 @@
+$(document).on('ready',function(){
+    $(".myCarousel").slick({
+
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 3,
+          speed: 200,
+          
+      });
+
+   });
